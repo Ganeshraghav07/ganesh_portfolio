f3473b6d244a95144f01f97698a0afbe58d35214
Ganesh Raghav <ganeshraghav07@gmail.com>
2025-04-20 16:44:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
+
 const form = document.querySelector('.contact-form');
 const successMessage = document.getElementById('success-message');
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  successMessage.textContent = "Message Sent Successfully!"; 
-  form.reset(); 
-  setTimeout(() => {
-    successMessage.textContent = "";
-  }, 3000);
-});
-
+if (form) {
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    successMessage.textContent = "Message Sent Successfully!";
+    form.reset();
+    setTimeout(() => {
+      successMessage.textContent = "";
+    }, 3000);
+  });
+}
 
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-});
+if (hamburger) {
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
+}
+
